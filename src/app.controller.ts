ae8080ecb93deb0be330f3398a4e41b515b459e9
you <you@you.com>
2021-11-23 17:55:31 +0800

    ok

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,12 +6,8 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-<<<<<<< HEAD
   getHelloNewByJohny6(): string {
     // add new line
-=======
-  getHelloNewByJohny5(): string {
->>>>>>> a69ebb44575631a725c771c70306ec09db605204
     return this.appService.getHello();
   }
 }
