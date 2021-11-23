46e36241aa507460c470a7f4fc0b1b2c933ff10b
you <you@you.com>
2021-11-23 23:03:24 +0800

    asd

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHelloNewByJohnyRemote1(): string {
+  getHelloNewByJohnyLocal(): string {
     return this.appService.getHello();
   }
 }
