d56bad7b3b1a5aa65730a763cd017d2ccbed05cf
you <you@you.com>
2021-11-23 16:38:18 +0800

    2nd commit

---
+++ product-controller.ts
@@ -0,0 +1,24 @@
+import { Body, Controller, Get, Post, Param } from "@nestjs/common";
+import { ProductService } from "./product.service";
+
+@Controller("products")
+export class ProductController {
+  constructor(private readonly productService: ProductService) {}
+
+  @Post()
+  addProduct(@Body("title") prodTitle: string): any {
+    const respId = this.productService.insertProduct(prodTitle);
+    return respId;
+  }
+
+  @Get()
+  getAllProducts() {
+    return this.productService.getAllProducts();
+  }
+
+  @Get(":id")
+  getProductById(@Param("id") prodId: string) {
+    //comment
+    return this.productService.getProductById(prodId);
+  }
+}
