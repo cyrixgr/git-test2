a73c1ff91e86873156d6f93cb4811840be77cd74
cyrixgr <edward.mail.gr@gmail.com>
2021-11-23 17:58:00 +0800

    Update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,8 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHelloNewByJohny6(): string {
-    // add new line
+  getHelloNewByJohny7(): string {
     return this.appService.getHello();
   }
 }
